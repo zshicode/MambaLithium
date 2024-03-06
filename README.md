ea8a0c660d3cc669924313fe3b25ccef4cf168cd
zshicode <calvinshi163@163.com>
2024-03-06 19:28:32 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# MambaLithium
- MambaLithium: Selective state space model for state-of-charge estimation of lithium-ion batteries
+# MambaStock: Selective state space model for stock prediction
+
+Mamba (Structured state space sequence models with selection mechanism and scan module, S6) has achieved remarkable success in sequence modeling tasks. This paper proposes a Mamba-based model to predict the stock price.
+
+## Requirements
+
+The code has been tested running under Python 3.7.4, with the following packages and their dependencies installed:
+```
+numpy==1.16.5
+matplotlib==3.1.0
+sklearn==0.21.3
+pandas==0.25.1
+pytorch==1.7.1
+```
+
+The stock data used in this repository was downloaded from [TuShare](https://tushare.pro/). The stock data on [TuShare](https://tushare.pro/) are with public availability. Some code of the Mamba model is from https://github.com/alxndrTL/mamba.py
+
+## Usage
+
+```
+python main.py
+```
+
+## Options
+
+We adopt an argument parser by package  `argparse` in Python, and the options for running code are defined as follow:
+
+```python
+parser = argparse.ArgumentParser()
+parser.add_argument('--use-cuda', default=False,
+                    help='CUDA training.')
+parser.add_argument('--seed', type=int, default=1, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Learning rate.')
+parser.add_argument('--wd', type=float, default=1e-5,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Dimension of representations')
+parser.add_argument('--layer', type=int, default=2,
+                    help='Num of layers')
+parser.add_argument('--n-test', type=int, default=300,
+                    help='Size of test set')
+parser.add_argument('--ts-code', type=str, default='601988',
+                    help='Stock code')                    
+
+args = parser.parse_args()
+args.cuda = args.use_cuda and torch.cuda.is_available()
+```
+
+## Citation
+
+```
+@article{shi2024mamba,
+  title={MambaStock: Selective state space model for stock prediction},
+  author={Zhuangwei Shi},
+  journal={arXiv preprint arXiv:2402.18959},
+  year={2024},
+}
+```
---
+++ main.py
@@ -0,0 +1,131 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mamba import Mamba, MambaConfig
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--use-cuda', default=False,
+                    help='CUDA training.')
+parser.add_argument('--seed', type=int, default=1, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Learning rate.')
+parser.add_argument('--wd', type=float, default=1e-5,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Dimension of representations')
+parser.add_argument('--layer', type=int, default=2,
+                    help='Num of layers')
+parser.add_argument('--test', type=str, default='FUDS',
+                    help='Test set')
+parser.add_argument('--temp', type=str, default='25',
+                    help='Temperature')                    
+
+args = parser.parse_args()
+args.cuda = args.use_cuda and torch.cuda.is_available()
+
+def evaluation_metric(y_test,y_hat):
+    MSE = mean_squared_error(y_test, y_hat)
+    RMSE = MSE**0.5
+    MAE = mean_absolute_error(y_test,y_hat)
+    R2 = r2_score(y_test,y_hat)
+    print('%.4f %.4f %.4f %.4f' % (MSE,RMSE,MAE,R2))
+
+def set_seed(seed,cuda):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
+
+set_seed(args.seed,args.cuda)
+
+class Net(nn.Module):
+    def __init__(self,in_dim,out_dim):
+        super().__init__()
+        self.config = MambaConfig(d_model=args.hidden, n_layers=args.layer)
+        self.mamba = nn.Sequential(
+            nn.Linear(in_dim,args.hidden),
+            Mamba(self.config),
+            Mamba(self.config),
+            nn.Linear(args.hidden,out_dim),
+            nn.Tanh()
+        )
+    
+    def forward(self,x):
+        x = self.mamba(x)
+        return x.flatten()
+
+def PredictWithData(trainX, trainy, testX):
+    clf = Net(len(trainX[0]),1)
+    opt = torch.optim.Adam(clf.parameters(),lr=args.lr,weight_decay=args.wd)
+    xt = torch.from_numpy(trainX).float().unsqueeze(0)
+    xv = torch.from_numpy(testX).float().unsqueeze(0)
+    yt = torch.from_numpy(trainy).float()
+    if args.cuda:
+        clf = clf.cuda()
+        xt = xt.cuda()
+        xv = xv.cuda()
+        yt = yt.cuda()
+    
+    for e in range(args.epochs):
+        clf.train()
+        z = clf(xt)
+        loss = F.mse_loss(z,yt)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        if e%10 == 0 and e!=0:
+            print('Epoch %d | Lossp: %.4f' % (e, loss.item()))
+
+    clf.eval()
+    mat = clf(xv)
+    if args.cuda: mat = mat.cpu()
+    yhat = mat.detach().numpy().flatten()
+    return yhat
+
+def ReadData(path,csv):
+    f = os.path.join(path,csv)
+    data = pd.read_csv(f)
+    tf = len(data)
+    data['Time'] = data.index
+    x = data[['Current','Voltage','Temperature']].values
+    y = data['SOC']+data['Time']/tf-1
+    y = y.values
+    return x,y
+
+def soc(y):
+    tf = len(y)
+    res = []
+    for i in range(tf):
+        res.append(1+y[i]-i/tf)
+    
+    return res
+
+path = './data/'+args.temp+'C'
+datal = ['DST','FUDS','US06']
+datal.remove(args.test)
+xt1, yt1 = ReadData(path,datal[0]+'_'+args.temp+'C.csv')
+xt2, yt2 = ReadData(path,datal[1]+'_'+args.temp+'C.csv')
+trainX = np.vstack((xt1,xt2))
+trainy = np.hstack((yt1,yt2))
+testX,testy = ReadData(path,args.test+'_'+args.temp+'C.csv')
+predictions = PredictWithData(trainX, trainy, testX)
+testy = soc(testy)
+predictions = soc(predictions)
+print('MSE RMSE MAE R2')
+evaluation_metric(testy, predictions)
+plt.figure()
+plt.plot(testy, label='True')
+plt.plot(predictions, label='Estimation')
+plt.title('SOC Estimation')
+plt.xlabel('Time(sec)')
+plt.ylabel('SOC value')
+plt.legend()
+plt.show()
